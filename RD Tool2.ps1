7e4402df85f002241044a8e895b1cdb507cf85ae
spdaniel89 <84418964+spdaniel89@users.noreply.github.com>
2021-05-26 11:45:57 -0500

    Add files via upload

---
+++ RD Tool2.ps1
@@ -0,0 +1,183 @@
+﻿function EnableRD{
+    $PC=$InputBox.text;
+    If ( $PC -eq 'Exit' )
+    {
+    [Environment]::Exit(0)
+    }
+    ElseIf ( $PC -eq '' )
+    {
+    $OutputBox.text = "You must enter a PC name." | Out-String
+    }
+    Else
+    {
+    $OutputBox.text = "Enabled!" | Out-String
+          invoke-command -ComputerName $PC -ScriptBlock {
+            Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 0
+            Enable-NetFirewallRule -DisplayGroup "Remote Desktop"
+            Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "Us erAuthentication" -Value 1
+        }        
+    }
+
+    
+}
+
+function ConnectRD{
+    $PC=$InputBox.text;
+    If ( $PC -eq 'Exit' )
+    {
+    [Environment]::Exit(0)
+    }
+    ElseIf ( $PC -eq '' )
+    {
+    $OutputBox.text = "You must enter a PC name." | Out-String
+    }
+    Else
+    {
+    $OutputBox.text = "Connecting..." | Out-String
+    mstsc /v:$PC
+    }
+}
+
+function DisableRD{
+    $PC=$InputBox.text;
+    If ( $PC -eq 'Exit' )
+    {
+    [Environment]::Exit(0)
+    }
+    ElseIf ( $PC -eq '' )
+    {
+    $OutputBox.text = "You must enter a PC name." | Out-String
+    }
+    Else
+    {
+    $OutputBox.text = "Disabled!" | Out-String
+    invoke-command -ComputerName $PC -ScriptBlock{
+        Disable-NetFirewallRule -DisplayGroup "Remote Desktop"
+        Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 1
+        }
+        }
+}
+
+function DisableExit{
+    $PC=$InputBox.text;
+     $OutputBox.text = "Disabled, exiting!" | Out-String
+    invoke-command -ComputerName $PC -ScriptBlock{
+        Disable-NetFirewallRule -DisplayGroup "Remote Desktop"
+        Set-ItemProperty -Path 'HKLM:\System\CurrentControlSet\Control\Terminal Server'-name "fDenyTSConnections" -Value 1
+        }
+[Environment]::Exit(0)
+}
+    
+
+
+###################### CREATING PS GUI TOOL #############################
+ 
+    #### Form settings #################################################################
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")  
+ 
+    $Form = New-Object System.Windows.Forms.Form
+    $Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
+    $Form.Text = "RD Enable and Connect"   
+    $Form.Size = New-Object System.Drawing.Size(720,360)  
+    $Form.StartPosition = "CenterScreen" #loads the window in the center of the screen
+    $Form.BackgroundImageLayout = "Zoom"
+    $Form.MinimizeBox = $False
+    $Form.MaximizeBox = $False
+    $Form.WindowState = "Normal"
+    $Form.SizeGripStyle = "Hide"
+    $Icon = [system.drawing.icon]::ExtractAssociatedIcon($PSHOME + "\powershell.exe")
+    $Form.Icon = $Icon
+ 
+    #### Title - Powershell GUI Tool ###################################################
+    $Label = New-Object System.Windows.Forms.Label
+    $LabelFont = New-Object System.Drawing.Font("Calibri",18,[System.Drawing.FontStyle]::Bold)
+    $Label.Font = $LabelFont
+    $Label.Text = "Enter PC name then click a button"
+    $Label.AutoSize = $True
+    $Label.Location = New-Object System.Drawing.Size(10,40) 
+    $Form.Controls.Add($Label)
+ 
+    #### Input window with "Computer name" label ##########################################
+    $InputBox = New-Object System.Windows.Forms.TextBox 
+    $InputBox.Location = New-Object System.Drawing.Size(375,50) 
+    $InputBox.Size = New-Object System.Drawing.Size(180,20) 
+    $Form.Controls.Add($InputBox)
+    $Label2 = New-Object System.Windows.Forms.Label
+    $Label2.Text = "Computer name:"
+    $Label2.AutoSize = $True
+    $Label2.Location = New-Object System.Drawing.Size(375,30) 
+    $Form.Controls.Add($Label2)
+ 
+###################### BUTTONS ##########################################################
+ 
+    #### Group boxes for buttons ########################################################
+    $groupBox = New-Object System.Windows.Forms.GroupBox
+    $groupBox.Location = New-Object System.Drawing.Size(10,95) 
+    $groupBox.size = New-Object System.Drawing.Size(685,100)
+    $groupBox.text = "Functions:"
+    $Form.Controls.Add($groupBox) 
+ 
+    #### Enable RD #################################################################
+    $EnableRD = New-Object System.Windows.Forms.Button
+    $EnableRD.Location = New-Object System.Drawing.Size(15,20)
+    $EnableRD.Size = New-Object System.Drawing.Size(150,60)
+    $EnableRD.Text = "EnableRD"
+    $EnableRD.Add_Click({EnableRD})
+    $EnableRD.Cursor = [System.Windows.Forms.Cursors]::Hand
+    $groupBox.Controls.Add($EnableRD)
+ 
+    #### Connect ###################################################################
+    $ConnectRD = New-Object System.Windows.Forms.Button
+    $ConnectRD.Location = New-Object System.Drawing.Size(180,20)
+    $ConnectRD.Size = New-Object System.Drawing.Size(150,60)
+    $ConnectRD.Text = "Connect Session"
+    $ConnectRD.Add_Click({ConnectRD})
+    $ConnectRD.Cursor = [System.Windows.Forms.Cursors]::Hand
+    $groupBox.Controls.Add($ConnectRD)
+ 
+    #### Disable RD ################################################################
+    $DisableRD = New-Object System.Windows.Forms.Button
+    $DisableRD.Location = New-Object System.Drawing.Size(345,20)
+    $DisableRD.Size = New-Object System.Drawing.Size(150,60)
+    $DisableRD.Text = "Disable RD"
+    $DisableRD.Add_Click({DisableRD})
+    $DisableRD.Cursor = [System.Windows.Forms.Cursors]::Hand
+    $groupBox.Controls.Add($DisableRD)
+
+    #### Disable RD and Exit########################################################
+    $DisableExit = New-Object System.Windows.Forms.Button
+    $DisableExit.Location = New-Object System.Drawing.Size(510,20)
+    $DisableExit.Size = New-Object System.Drawing.Size(150,60)
+    $DisableExit.Text = "Disable RD and Exit"
+    $DisableExit.Add_Click({DisableExit})
+    $DisableExit.Cursor = [System.Windows.Forms.Cursors]::Hand
+    $groupBox.Controls.Add($DisableExit)
+ 
+###################### END BUTTONS ######################################################
+ 
+    #### Output Box Field ###############################################################
+    $outputBox = New-Object System.Windows.Forms.RichTextBox
+    $outputBox.Location = New-Object System.Drawing.Size(10,215) 
+    $outputBox.Size = New-Object System.Drawing.Size(685,100)
+    $outputBox.Font = New-Object System.Drawing.Font("Consolas", 12 ,[System.Drawing.FontStyle]::Regular)
+    $outputBox.MultiLine = $True
+    $outputBox.ScrollBars = "Vertical"
+    $outputBox.Text = " `
+    Status"
+    $Form.Controls.Add($outputBox)
+ 
+    ##############################################
+ 
+    $Form.Add_Shown({$Form.Activate()})
+    [void] $Form.ShowDialog()
+
+
+
+
+
+
+
+
+
+
---
+++ Reporting.ps1
@@ -0,0 +1,101 @@
+﻿function report{
+          $PC=$InputBox.text;
+          $date = Get-Date -Format "MMddyy-HHmm";
+          invoke-command -ComputerName $PC -ScriptBlock {
+          Get-ItemProperty HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | Select-Object DisplayName, DisplayVersion, Publisher, InstallDate | Sort-Object DisplayName | Export-CSV -NoTypeInformation -Path C:\"$Using:PC$Using:date".csv;
+          }
+          Copy-Item -Path \\"$PC"\c$\"$PC$date".csv -Destination C:\Reports\
+          Remove-Item -Path \\"$PC"\c$\"$PC$date".csv
+          If ( Test-Path -Path C:\Reports\"$PC$date".csv -PathType Leaf)
+          {$outputBox.Text = "$PC$date.csv saved to c:\Reports\" | Out-String}
+          Else
+          {$outputBox.Text = "Error! Check your entry."}
+          }
+
+function ShowPrinters{
+    $PC=$InputBox.text;
+    $printers = get-printer -computername $PC | Select-Object Name, PortName | Sort-Object Name | Out-String;
+    $OutputBox.Text = "$printers"
+    }
+
+
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Drawing") 
+    [void] [System.Reflection.Assembly]::LoadWithPartialName("System.Windows.Forms")  
+
+    $Form = New-Object System.Windows.Forms.Form
+    $Form.FormBorderStyle = [System.Windows.Forms.FormBorderStyle]::FixedSingle
+    $Form.Text = "Pull Remote System Report"   
+    $Form.Size = New-Object System.Drawing.Size(720,900)  
+    $Form.StartPosition = "CenterScreen" #loads the window in the center of the screen
+    $Form.BackgroundImageLayout = "Zoom"
+    $Form.MinimizeBox = $False
+    $Form.MaximizeBox = $False
+    $Form.WindowState = "Normal"
+    $Form.SizeGripStyle = "Hide"
+    $Icon = [system.drawing.icon]::ExtractAssociatedIcon($PSHOME + "\powershell.exe")
+    $Form.Icon = $Icon
+    
+    #### Title - Powershell GUI Tool ###################################################
+    $Label = New-Object System.Windows.Forms.Label
+    $LabelFont = New-Object System.Drawing.Font("Calibri",18,[System.Drawing.FontStyle]::Bold)
+    $Label.Font = $LabelFont
+    $Label.Text = "Enter PC name then click a button"
+    $Label.AutoSize = $True
+    $Label.Location = New-Object System.Drawing.Size(10,40) 
+    $Form.Controls.Add($Label)
+ 
+    #### Input window with "Computer name" label ##########################################
+    $InputBox = New-Object System.Windows.Forms.TextBox 
+    $InputBox.Location = New-Object System.Drawing.Size(375,50) 
+    $InputBox.Size = New-Object System.Drawing.Size(180,20) 
+    $Form.Controls.Add($InputBox)
+    $Label2 = New-Object System.Windows.Forms.Label
+    $Label2.Text = "Computer name:"
+    $Label2.AutoSize = $True
+    $Label2.Location = New-Object System.Drawing.Size(375,30) 
+    $Form.Controls.Add($Label2)
+ 
+    ##########BUTTONS##############
+    $groupBox = New-Object System.Windows.Forms.GroupBox
+    $groupBox.Location = New-Object System.Drawing.Size(10,95)
+    $groupBox.size = New-Object System.Drawing.Size(685,100)
+    $groupBox.text = "Functions:"
+    $Form.Controls.Add($groupBox)
+
+    #########Pull Report###########
+    $report = New-Object System.Windows.Forms.Button
+    $report.Location = New-Object System.Drawing.Size(15,20)
+    $report.size = New-Object System.Drawing.Size(150,60)
+    $report.text = "Pull Report"
+    $report.Add_Click({report})
+    $report.Cursor = [System.Windows.Forms.Cursors]::Hand
+    $groupBox.Controls.Add($report)
+
+
+    #### Show Printers #############
+    $ShowPrinters = New-Object System.Windows.Forms.Button
+    $ShowPrinters.Location = New-Object System.Drawing.Size(180,20)
+    $ShowPrinters.Size = New-Object System.Drawing.Size(150,60)
+    $ShowPrinters.Text = "Show Printers"
+    $ShowPrinters.Add_Click({ShowPrinters})
+    $ShowPrinters.Cursor = [System.Windows.Forms.Cursors]::Hand
+    $groupBox.Controls.Add($ShowPrinters)
+
+###################### END BUTTONS ######################################################
+ 
+    #### Output Box Field ###############################################################
+    $outputBox = New-Object System.Windows.Forms.RichTextBox
+    $outputBox.Location = New-Object System.Drawing.Size(10,215) 
+    $outputBox.Size = New-Object System.Drawing.Size(685,640)
+    $outputBox.Font = New-Object System.Drawing.Font("Consolas", 12 ,[System.Drawing.FontStyle]::Regular)
+    $outputBox.MultiLine = $True
+    $outputBox.ScrollBars = "Vertical"
+    $outputBox.Text = " `
+    "
+    $Form.Controls.Add($outputBox)
+ 
+    ##############################################
+ 
+    $Form.Add_Shown({$Form.Activate()})
+    [void] $Form.ShowDialog()
+
